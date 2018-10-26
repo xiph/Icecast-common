ae0a77ebdecf31165e1b5e0ebf488eed620e5730
Marvin Scholz <epirat07@gmail.com>
2018-10-26 13:59:15 +0200

    Add igloo.h

---
+++ include/igloo.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2018   Marvin Scholz <epirat07@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#ifndef IGLOO_H
+# define IGLOO_H 1
+/**
+ * @file
+ * Put a good description of this file here
+ */
+
+/* Put stuff here */
+
+#endif /* ! IGLOO_H */
